666c8f6004bdddbfb1f85cf30984ace515e73ec2
Vladyslav Riabchuk <vladyslav.riabchuk@4experience.co>
2023-03-01 22:51:50 +0100

    Create project constants string container

---
+++ 2D_Popup_System/Assets/Scripts/ProjectConstants.cs
@@ -0,0 +1,8 @@
+namespace PopupSystem
+{
+    public static class ProjectConstants
+    {
+        public const string ROOT_MENU_PATH = "Popup System/";
+        public const string POPUP_MENU_PATH = ROOT_MENU_PATH + "Popup Settings";
+    }
+}
