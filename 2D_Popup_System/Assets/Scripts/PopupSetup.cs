e05e14f6b1929688898442335ad04e899d93c580
Vladyslav Riabchuk <vladyslav.riabchuk@4experience.co>
2023-03-02 01:20:50 +0100

    Update popup setup: add parameters

--- 2D_Popup_System/Assets/Scripts/PopupSetup.cs
+++ 2D_Popup_System/Assets/Scripts/PopupSetup.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 
 namespace PopupSystem
 {
-    [CreateAssetMenu( menuName = ProjectConstants.POPUP_MENU_PATH + ASSET_NAME)]
+    [CreateAssetMenu(menuName = ProjectConstants.POPUP_MENU_PATH + ASSET_NAME)]
     public class PopupSetup : ScriptableObject
     {
+        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_FIELD_LENGTH + "12")]
+        public string Title { get; private set; }
+        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_FIELD_LENGTH + "75")]
+        public string Message { get; private set; }
+        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_FIELD_LENGTH + "12")]
+        public string ButtonLable { get; private set; }
         [field: SerializeField]
-        public string Title { get; set; }
+        public string ButtonImageAddress { get; private set; }
         [field: SerializeField]
-        public string Message { get; set; }
-        [field: SerializeField]
-        public string ButtonImageAddress { get; set; }
-        [field: SerializeField]
-        public string BackgroundImageAddress { get; set; }
-        
+        public string BackgroundImageAddress { get; private set; }
+
+        public int MaxTitleLength { get; private set; } = 12;
+        public int MaxMessageLength { get; private set; } = 75;
+        public int MaxButtonLableLength { get; private set; } = 12;
+
         private const string ASSET_NAME = nameof(PopupSetup);
     }
 }
