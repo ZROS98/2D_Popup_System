b7b4dd601c0adbf47c9414730afa3e50dab58fdf
Vladyslav Riabchuk <vladyslav.riabchuk@4experience.co>
2023-03-02 01:22:50 +0100

    Add constant headers for references and maxt text length

--- 2D_Popup_System/Assets/Scripts/PopupSetup.cs
+++ 2D_Popup_System/Assets/Scripts/PopupSetup.cs
@@ -5,11 +5,11 @@ namespace PopupSystem
     [CreateAssetMenu(menuName = ProjectConstants.POPUP_MENU_PATH + ASSET_NAME)]
     public class PopupSetup : ScriptableObject
     {
-        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_FIELD_LENGTH + "12")]
+        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_LENGTH + "12")]
         public string Title { get; private set; }
-        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_FIELD_LENGTH + "75")]
+        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_LENGTH + "75")]
         public string Message { get; private set; }
-        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_FIELD_LENGTH + "12")]
+        [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_LENGTH + "12")]
         public string ButtonLable { get; private set; }
         [field: SerializeField]
         public string ButtonImageAddress { get; private set; }
--- 2D_Popup_System/Assets/Scripts/ProjectConstants.cs
+++ 2D_Popup_System/Assets/Scripts/ProjectConstants.cs
@@ -4,5 +4,8 @@ namespace PopupSystem
     {
         public const string ROOT_MENU_PATH = "Popup System/";
         public const string POPUP_MENU_PATH = ROOT_MENU_PATH + "Popup Settings/";
+        
+        public const string HEADER_REFERENCES = "References";
+        public const string HEADER_MAX_TEXT_LENGTH = "Max text length: ";
     }
 }
