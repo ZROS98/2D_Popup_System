a4ed4d8ee475585ebd0354ec26615265a393aa2d
Vladyslav Riabchuk <vladyslav.riabchuk@4experience.co>
2023-03-03 02:29:14 +0100

    Create BasicPopupController

---
+++ 2D_Popup_System/Assets/Scripts/BasicPopupController.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+namespace PopupSystem
+{
+    public class BasicPopupController : MonoBehaviour
+    {
+        protected virtual void Start ()
+        {
+        }
+
+        protected void SetTextReferences (TMP_Text tmpText, string targetText, int maxTextLength)
+        {
+            tmpText.text = GetProperTextLength(targetText, maxTextLength);
+        }
+
+        private string GetProperTextLength (string targetText, int maxTextLenght)
+        {
+            return targetText.Length > maxTextLenght ? targetText.Substring(0, maxTextLenght) : targetText;
+        }
+    }
+}
