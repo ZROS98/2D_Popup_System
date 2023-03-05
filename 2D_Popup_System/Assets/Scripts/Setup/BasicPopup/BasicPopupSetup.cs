104ecef57304b13af3b7113ba4fcbed188c3d91e
Vladyslav Riabchuk <vladyslav.riabchuk@4experience.co>
2023-03-05 19:37:19 +0100

    Remove ITitle from dependencies

--- 2D_Popup_System/Assets/Scripts/Setup/BasicPopup/BasicPopupSetup.cs
+++ 2D_Popup_System/Assets/Scripts/Setup/BasicPopup/BasicPopupSetup.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace PopupSystem.Data
 {
     [CreateAssetMenu(menuName = ProjectConstants.POPUP_MENU_PATH + ASSET_NAME)]
-    public class BasicPopupSetup : ScriptableObject, ITitle
+    public class BasicPopupSetup : ScriptableObject
     {
         [field: SerializeField, Header(ProjectConstants.HEADER_MAX_TEXT_LENGTH + "12")]
         public string Title { get; set; }
