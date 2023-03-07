eba6295f8073d27f35e97c4a28dca63c883cea52
Vladyslav Riabchuk <vladyslav.riabchuk@4experience.co>
2023-03-07 01:38:38 +0100

    Project cleanup

--- 2D_Popup_System/Assets/Scripts/PopupControllers/ScheduledPopupController.cs
+++ 2D_Popup_System/Assets/Scripts/PopupControllers/ScheduledPopupController.cs
@@ -10,11 +10,11 @@ namespace PopupSystem
         public SchedulePopupSetup CurrentSchedulePopupSetup { get; set; }
         
         [field: SerializeField]
-        private TMP_Text CurrentTitleText { get; set; }
+        private TMP_Text TitleText { get; set; }
         [field: SerializeField]
-        private TMP_Text CurrentMessageText { get; set; }
+        private TMP_Text MessageText { get; set; }
         [field: SerializeField]
-        private TMP_Text CurrentButtonLabelText { get; set; }
+        private TMP_Text ButtonLabelText { get; set; }
         
         protected override void Awake ()
         {
@@ -24,9 +24,9 @@ namespace PopupSystem
         
         private void SetReferences ()
         {
-            SetTextReferences(CurrentTitleText, CurrentSchedulePopupSetup.Title, CurrentSchedulePopupSetup.MaxTitleLength);
-            SetTextReferences(CurrentMessageText, CurrentSchedulePopupSetup.Message, CurrentSchedulePopupSetup.MaxMessageLength);
-            SetTextReferences(CurrentButtonLabelText, CurrentSchedulePopupSetup.ButtonLabel, CurrentSchedulePopupSetup.MaxButtonLabelLength);
+            SetTextReferences(TitleText, CurrentSchedulePopupSetup.Title, CurrentSchedulePopupSetup.MaxTitleLength);
+            SetTextReferences(MessageText, CurrentSchedulePopupSetup.Message, CurrentSchedulePopupSetup.MaxMessageLength);
+            SetTextReferences(ButtonLabelText, CurrentSchedulePopupSetup.ButtonLabel, CurrentSchedulePopupSetup.MaxButtonLabelLength);
         }
     }
 }
